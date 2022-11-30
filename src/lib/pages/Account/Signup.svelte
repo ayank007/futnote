5075384b1d78187df2c6aca19a4897cbaa12c18c
Ayan Koley <ayankoley@gmail.com>
2022-11-30 18:35:54 +0530

    nothing basiclly

---
+++ src/lib/pages/Account/Signup.svelte
@@ -0,0 +1,32 @@
+<form action="">
+    <h1>Sign Up</h1>
+    <label>
+        <p>Username</p>
+        <input type="text" name="name" placeholder="John Doe" />
+    </label>
+    <label>
+        <p>Email ID</p>
+        <input type="email" name="email" placeholder="john.doe@gmail.com" />
+    </label>
+    <label>
+        <p>Password</p>
+        <input type="password" name="password" id="********">
+    </label>
+    <button type="submit">Create Account</button>
+</form>
+
+<style lang="scss">
+    p{
+        font-size: 18px;
+        font-weight: bold;
+    }
+    input{
+        background-color: var(--blueLight);
+        height: 40px;
+        border-radius: 4px;
+        padding: 20px 0;
+        &::placeholder{
+            color: rgba($color: #000000, $alpha: 0.6);
+        }
+    }
+</style>
